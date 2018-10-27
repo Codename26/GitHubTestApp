f1ce07dea0e456c22ffe0e862569f0a499296c58
Codename26 <slava.burchenko@gmail.com>
2018-10-27 13:26:35 +0300

    Added Login sreen

--- app/src/main/java/com/codename26/githubtestapp/LoginActivity.java
+++ app/src/main/java/com/codename26/githubtestapp/LoginActivity.java
@@ -3,7 +3,7 @@ package com.codename26.githubtestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
