f67d2a41bc2c4752a53f28b89f47d82ea2c9c4dd
Codename26 <slava.burchenko@gmail.com>
2018-10-27 17:20:41 +0300

    Added retrofit login

---
+++ app/src/main/java/com/codename26/githubtestapp/GitHubApp.java
@@ -0,0 +1,71 @@
+package com.codename26.githubtestapp;
+
+import android.app.Application;
+
+import com.codename26.githubtestapp.data.remote.GitHubService;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import okhttp3.ConnectionSpec;
+import okhttp3.Credentials;
+import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
+
+public class GitHubApp extends Application {
+
+    public static final String REPOS_LIST = "REPOS_LIST";
+    private static final String BASE_URL = "https://api.github.com";
+
+    private String authToken;
+    private final String username = "codename2611";
+    private final String password = "pbrv8S04215";
+    private Retrofit retrofit;
+    private static GitHubService gitHubService;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Timber.plant(new Timber.DebugTree());
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+// set your desired log level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        clientBuilder.addInterceptor(logging);
+        clientBuilder.connectionSpecs(Collections.singletonList(ConnectionSpec.MODERN_TLS));
+        authToken = Credentials.basic(username, password);
+
+        Interceptor headerAuthorizationInterceptor = new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                okhttp3.Request request = chain.request();
+                Headers headers = request.headers().newBuilder().add("Authorization", authToken).build();
+                request = request.newBuilder().headers(headers).build();
+                return chain.proceed(request);
+            }
+        };
+
+        clientBuilder.addInterceptor(headerAuthorizationInterceptor);
+
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(clientBuilder.build())
+                .build();
+        gitHubService = retrofit.create(GitHubService.class);
+    }
+
+    public static GitHubService getGitHubService(){
+        return gitHubService;
+    }
+
+
+}
--- app/src/main/java/com/codename26/githubtestapp/LoginActivity.java
+++ app/src/main/java/com/codename26/githubtestapp/LoginActivity.java
@@ -1,7 +1,23 @@
 package com.codename26.githubtestapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.codename26.githubtestapp.data.remote.GitHubService;
+import com.codename26.githubtestapp.model.Repo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
+
+import static com.codename26.githubtestapp.GitHubApp.REPOS_LIST;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +25,41 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GitHubService gitHubService = GitHubApp.getGitHubService();
+
+        gitHubService.getRepos()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(repo -> Timber.d("repos: " + repo))
+                .doOnError(e -> Timber.d("error: " + e))
+                .subscribe(new Observer<ArrayList<Repo>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ArrayList<Repo> repos) {
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelableArrayList(REPOS_LIST ,repos);
+                    startActivity(new Intent(LoginActivity.this, ListActivity.class));
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(LoginActivity.this, "Login failed. Please, check your credentials", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
